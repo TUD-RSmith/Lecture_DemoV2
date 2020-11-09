3f0d1aa8daa437bede2baf143e050f32d0a47db6
TUD-RSmith <71321440+TUD-RSmith@users.noreply.github.com>
2020-11-09 11:42:18 +0000

    Inital Commit

---
+++ Button_Demo/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string URL = "https://github.com/TUD-RSmith/Lecture_Demo";
+    public void GoToSene1()
+    {
+        SceneManager.LoadScene("scene1");
+    }
+
+    public void GoToSene2()
+    {
+        SceneManager.LoadScene("scene2");
+    }
+
+    public void loadUrl()
+    {
+        Application.OpenURL(URL);
+    }
+}
